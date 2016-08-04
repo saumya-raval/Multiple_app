b90cdf301ca9d4fe69a8da3e236aae3482cb23bc
Saumya Raval <saumya_v_raval@yahoo.com>
2016-08-04 15:19:46 -0700

    Initial Commit

---
+++ Multiples_app_newTests/Multiples_app_newTests.swift
@@ -0,0 +1,36 @@
+//
+//  Multiples_app_newTests.swift
+//  Multiples_app_newTests
+//
+//  Created by Saumya Raval on 8/4/16.
+//  Copyright © 2016 Saumya Raval. All rights reserved.
+//
+
+import XCTest
+@testable import Multiples_app_new
+
+class Multiples_app_newTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
